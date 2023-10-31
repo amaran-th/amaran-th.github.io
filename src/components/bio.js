f5003663b8d00b5298729dae3830b25aa03e6d94
amaran-th <songsy405@naver.com>
2023-10-31 13:58:54 +0900

    post: 일일 방문자 수 통계 뱃지 추가

--- src/components/bio.js
+++ src/components/bio.js
@@ -114,6 +114,7 @@ const Bio = () => {
               </li>
             </ul>
           </div>
+          <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Famaran-th.github.io&count_bg=%230C3675&title_bg=%23567EB9&icon=octopusdeploy.svg&icon_color=%23E7E7E7&title=%EC%9D%BC%EC%9D%BC+%EB%B0%A9%EB%AC%B8%EC%9E%90+%EC%88%98&edge_flat=false" /></a>
         </div>
       )}
     </div>
